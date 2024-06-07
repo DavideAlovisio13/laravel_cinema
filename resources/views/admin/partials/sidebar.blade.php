aecfafb09582663fd5823b59fd49a26a4324dc3c
sashapoliti <sas.politi@gmail.com>
2024-06-08 01:34:09 +0200

    user section

--- resources/views/admin/partials/sidebar.blade.php
+++ resources/views/admin/partials/sidebar.blade.php
@@ -2,6 +2,43 @@
     <div class="logo text-center">
         <a href="#" class="nav-link text-uppercase">nome/logo cinema</a>
     </div>
+    <div class="user">
+        <div class="profile">
+            <a class="nav-link d-flex align-items-center collapsed" href="#collapseExample" data-bs-toggle="collapse"
+                aria-expanded="false">
+                <img class="rounded-circle profile-picture me-2"
+                    src="https://avatarfiles.alphacoders.com/373/373447.jpg"
+                    alt="{{ Auth::user()->name }} profile picture">
+                <span>{{ Auth::user()->name }}</span>
+                <i class="fa-solid fa-caret-down ms-auto"></i>
+            </a>
+            <div class="collapse" id="collapseExample">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ url('profile') }}">
+                            <i class="fa-solid fa-user"></i> {{ __('Profile') }}
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">
+                            <i class="fa-solid fa-gear"></i> Settings
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('logout') }}"
+                            onclick="event.preventDefault();
+                                             document.getElementById('logout-form').submit();">
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                @csrf
+                            </form>
+                            <i class="fa-solid fa-sign-out"></i> {{ __('Logout') }}
+                        </a>
+                    </li>
+                </ul>
+            </div>
+
+        </div>
+    </div>
     {{-- <a href="/" class="nav-link text-white">
         <h2 class="p-2">
             <i class="fa-solid fa-square-rss"></i> Boolpress
