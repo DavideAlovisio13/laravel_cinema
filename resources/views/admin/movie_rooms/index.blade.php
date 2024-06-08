a3886d9678c834bb95dfbc0b5f601fa727463cfa
sashapoliti <sas.politi@gmail.com>
2024-06-08 12:49:14 +0200

    add actions button in projections (no worked)

--- resources/views/admin/movie_rooms/index.blade.php
+++ resources/views/admin/movie_rooms/index.blade.php
@@ -11,6 +11,7 @@
                         <th scope="col" class="text-danger bg-transparent">Room</th>
                         <th scope="col" class="text-danger bg-transparent">Ticket Price</th>
                         <th scope="col" class="text-danger bg-transparent">Seats</th>
+                        <th scope="col" class="text-danger bg-transparent">Actions</th>
                     </tr>
                 </thead>
                 <tbody class="table-group-divider ">
@@ -21,6 +22,22 @@
                             <td class="bg-transparent text-black border-bottom-0">{{ $projection->room->name }}</td>
                             <td class="bg-transparent text-black border-bottom-0">{{ $projection->ticket_price }}</td>
                             <td class="bg-transparent text-black border-bottom-0">{{ $projection->room->seats }}</td>
+                            <td
+                                    class=" bg-transparent text-black d-flex border-bottom-0 flex-column justify-content-center align-items-center">
+                                    <a href="{{ route('admin.movie_rooms.show', $projection->id) }}"><i
+                                            class="fa-solid text-danger fa-eye"></i></a>
+                                    <a href="{{ route('admin.movie_rooms.edit', $projection->id) }}"><i
+                                            class="fa-solid text-danger fa-pen"></i></a>
+                                    <form action="{{ route('admin.movie_rooms.destroy', $projection->id) }}" method="POST"
+                                        class="d-inline-block">
+                                        @csrf
+                                        @method('DELETE')
+                                        <button type="submit" class="delete-button border-0 bg-transparent"
+                                            data-item-title="{{ $projection->title }}">
+                                            <i class="fa-solid text-danger fa-trash"></i>
+                                        </button>
+                                    </form>
+                                </td>
                         </tr>
                     @endforeach
                 </tbody>
