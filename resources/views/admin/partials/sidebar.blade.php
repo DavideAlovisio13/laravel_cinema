323680d58ca481cb1c1a35e53f2f8c5da4bbf850
sashapoliti <sas.politi@gmail.com>
2024-06-09 16:42:46 +0200

    add little detail after link for current route

--- resources/views/admin/partials/sidebar.blade.php
+++ resources/views/admin/partials/sidebar.blade.php
@@ -40,61 +40,29 @@
     </div>
     <ul id="routes-list" class="navbar-nav">
         <li>
-            <a href="{{ route('admin.dashboard') }}" class="nav-link d-flex align-items-center">
+            <a href="{{ route('admin.dashboard') }}" class="{{ Route::is('admin.dashboard') ? 'active' : '' }} nav-link d-flex align-items-center">
                 <i class="fa-solid fa-building-columns"></i> Dashboard
             </a>
         </li>
         <li>
-            <a href="{{ route('admin.movies.index') }}" class="nav-link d-flex align-items-center">
+            <a href="{{ route('admin.movies.index') }}" class="{{ Route::is('admin.movies.*') ? 'active' : '' }} nav-link d-flex align-items-center">
                 <i class="fa-solid fa-film"></i> Movies
             </a>
         </li>
         <li>
-            <a href="{{ route('admin.rooms.index') }}" class="nav-link d-flex align-items-center">
+            <a href="{{ route('admin.rooms.index') }}" class="{{ Route::is('admin.rooms.*') ? 'active' : '' }} nav-link d-flex align-items-center">
                 <i class="fa-solid fa-person-booth"></i> Rooms
             </a>
         </li>
         <li>
-            <a href="{{ route('admin.movie_rooms.index') }}" class="nav-link d-flex align-items-center">
+            <a href="{{ route('admin.movie_rooms.index') }}" class="{{ Route::is('admin.movie_rooms.*') ? 'active' : '' }} nav-link d-flex align-items-center">
                 <i class="fa-solid fa-video"></i> Projections
             </a>
         </li>
         <li>
-            <a href="{{ route('admin.slots.index') }}" class="nav-link d-flex align-items-center">
+            <a href="{{ route('admin.slots.index') }}" class="{{ Route::is('admin.slots.*') ? 'active' : '' }} nav-link d-flex align-items-center">
                 <i class="fa-solid fa-clock"></i> Programmations
             </a>
         </li>
     </ul>
-    {{-- <a href="/" class="nav-link text-white">
-        <h2 class="p-2">
-            <i class="fa-solid fa-square-rss"></i> Boolpress
-        </h2>
-    </a>
-    <ul class="nav flex-column">
-        <li class="nav-item">
-            <a class="nav-link text-white {{ Route::currentRouteName() == 'admin.dashboard' ? 'active' : '' }}"
-                href="{{ route('admin.dashboard') }}">
-                <i class="fa-solid fa-tachometer-alt fa-lg fa-fw"></i> Dasboard
-            </a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link  text-white {{ Route::currentRouteName() == 'admin.projects.index' ? 'active' : '' }}"
-                href="#">
-                <i class="fa-solid fa-newspaper fa-lg fa-fw"></i> Projects
-            </a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link  text-white {{ Route::currentRouteName() == 'admin.types.index' ? 'active' : '' }}"
-                href="#">
-                <i class="fa-solid fa-newspaper fa-lg fa-fw"></i> Types
-            </a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link  text-white {{ Route::currentRouteName() == 'admin.technologies.index' ? 'active' : '' }}"
-                href="#">
-                <i class="fa-solid fa-tag fa-lg fa-fw"></i> Technologies
-            </a>
-        </li>
-
-    </ul> --}}
 </nav>
