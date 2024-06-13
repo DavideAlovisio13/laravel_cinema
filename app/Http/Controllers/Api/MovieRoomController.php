4738dca4abd551feb6c52eba30e015ea635a75ef
Nik0-9 <filannino.nicola9@gmail.com>
2024-06-13 17:57:27 +0200

    ADDED api route and controller Room and MovieRoom

---
+++ app/Http/Controllers/Api/MovieRoomController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Models\MovieRoom;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+
+class MovieRoomController extends Controller
+{
+    public function index()
+    {
+        $projections = MovieRoom::with('movie', 'room', 'slot')->get();
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Projections retrieved successfully',
+            'results' => $projections
+        ], 200);
+    }
+}
+
